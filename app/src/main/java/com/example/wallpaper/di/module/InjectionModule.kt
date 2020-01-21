6d4b2610df21f3d48b87e24961244de9ff804509
ambarkumarpandey <ambar.ara123@gmail.com>
2020-01-21 12:15:33 +0530

    added viewModel and repository module

--- app/src/main/java/com/example/wallpaper/di/module/InjectionModule.kt
+++ app/src/main/java/com/example/wallpaper/di/module/InjectionModule.kt
@@ -1,5 +1,10 @@
 package com.example.wallpaper.di.module
 
+import com.example.wallpaper.data.MainRepository
+import com.example.wallpaper.ui.detail.DetailViewModel
+import com.example.wallpaper.ui.main.MainViewModel
+import com.example.wallpaper.ui.rx.RxViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val networkModule = module {
@@ -17,3 +22,16 @@ val managerModule = module {
 
     single { AppModule.provideWallpaperManager(get()) }
 }
+
+val viewModelModule = module {
+    viewModel { MainViewModel(get()) }
+
+    viewModel { DetailViewModel(get(),get()) }
+
+    viewModel { RxViewModel() }
+}
+
+val repositoryModule = module {
+    factory { MainRepository(get()) }
+}
+
